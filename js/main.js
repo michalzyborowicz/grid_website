4ed2fa5e0d66597e8299d2102fb74560818db31f
Michal Zyborowicz <mt.zyborowicz@gmail.com>
2022-01-29 14:48:46 +0100

    website is finished, just fixing small javaScript navigation issues

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 const burgerBtn = document.querySelector('.hamburger')
 const navMob = document.querySelector('.nav-mobile')
 const footerYear = document.querySelector('.footer__year')
-
-
+const navLink = document.querySelectorAll('a.nav__link')
 
 const handleCurrenYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+
+
+function enableScroll() {
+    window.onscroll = function() {};
 }
 
 const navSlide = () => {
-burgerBtn.classList.toggle('is-active')
+	burgerBtn.classList.toggle('is-active')
 
-    navMob.classList.toggle('nav-mobile--active')
+	navMob.classList.toggle('nav-mobile--active')
+
+
+}
+const navRemover = () => {
+	navMob.classList.remove('nav-mobile--active')
 }
 
-
-
-
-
-
 burgerBtn.addEventListener('click', navSlide)
-
-
-
+//window.addEventListener('scroll', navSlide)
+navLink.addEventListener('click', navSlide)
 
 handleCurrenYear()
